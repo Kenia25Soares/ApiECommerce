ad873ebe7314ae91475054b87a13751d20304e96
Kenia25Soares <keniadefreitasmorais@gmail.com>
2025-06-09 11:37:41 +0100

    Add project files.

---
+++ ApiECommerce/Entities/User.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiECommerce.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        //[Required]
+        [StringLength(100)]
+        public string? Name { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string? Email { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string? Password { get; set; }
+
+        [StringLength(100)]
+        public string? UrlImage { get; set; }
+
+        [StringLength(80)]
+        public string? Phone { get; set; }
+
+        [NotMapped]  //indica que a imagem não será mapeada para o banco de dados
+        public IFormFile? Image { get; set; }
+
+        public ICollection<Order>? Orders { get; set; }
+    }
+}
